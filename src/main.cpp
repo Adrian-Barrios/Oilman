4e614379ba4f81bd9374ba764cae9e40deabf7f6
Adrian Barrios <adrianbarrioscortez@gmail.com>
2026-07-17 14:40:53 +0200

    Configured ImGui and rlImGui for rendering these config windows. Initialized a sample window within the main loop.
    Requried setup context rlImGuiSetup(true).

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include "raylib.h"
-
+#include "imgui.h"
+#include <rlImGui.h>
 int main()
 {
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 	SetTargetFPS(30);
 	InitWindow(800, 450, "Oilman");
+	rlImGuiSetup(true); // Required setup context. Won't work without this.
 
 	int posX = 30;
 	int posY = 200;
@@ -16,12 +18,29 @@ int main()
 		BeginDrawing();
 		ClearBackground(BLACK);
 
+		rlImGuiBegin(); // ImGui window
+
+		ImGui::Begin("Test");
+		ImGui::Text("ImGui visible test");
+		ImGui::Button("Click me");
+		ImGui::End();
+
+		Color c;
+		c.r = 255;
+		c.g = 0;
+		c.b = 200;
+		c.a = 255;
+
 		DrawText("Oilman", 350, 50, 50, RED);
 		DrawRectangle(posX, posY, size, size, BLUE);
 		posX += 1;
 
+		rlImGuiEnd();
+
 		EndDrawing();
 	}
+
+	rlImGuiShutdown();
 	CloseWindow();
 	return 0;
 }
