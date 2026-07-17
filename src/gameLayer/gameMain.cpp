96f2e3c56ae996b66df96a3f6f15d7b984201225
Adrian Barrios <adrianbarrioscortez@gmail.com>
2026-07-17 22:28:03 +0200

    Camera implementation to move around the map.
    Steps taken:
    1. Camera2D object within my GameData struct.
    2. Configure the cameras origin, rotation and zoom.
    3. Calculate the camera offset of updateGane
    4. Assigne keys to camera movement
    5. Start and end camera through BeginMode2D(gameData.camera)

--- src/gameLayer/gameMain.cpp
+++ src/gameLayer/gameMain.cpp
@@ -7,6 +7,7 @@
 struct GameData
 {
 	GameMap gameMap;
+	Camera2D camera;
 
 } gameData;
 
@@ -25,6 +26,12 @@ bool initGame()
 	gameData.gameMap.getBlocUnsafe(4, 4).type = Block::dirt;
 	#pragma endregion
 
+	#pragma region camera
+	gameData.camera.target = { 0,0 };
+	gameData.camera.rotation = 0.0f;
+	gameData.camera.zoom = 100.0f;
+	#pragma endregion
+
 	return true;
 }
 bool updateGame()
@@ -32,7 +39,20 @@ bool updateGame()
 
 	float deltaTime = GetFrameTime();
 	if (deltaTime > 1.f / 5) { deltaTime = 1 / 5.f; }
+
+	gameData.camera.offset = { GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f };
+
 	ClearBackground({ 75,75,150,255 });
+
+	BeginMode2D(gameData.camera);
+
+	#pragma region cameraMovement
+	if (IsKeyDown(KEY_LEFT)) gameData.camera.target.x -= 7.f * deltaTime;
+	if (IsKeyDown(KEY_RIGHT)) gameData.camera.target.x +=7.f * deltaTime;
+	if (IsKeyDown(KEY_DOWN)) gameData.camera.target.y += 7.f * deltaTime;
+	if (IsKeyDown(KEY_UP)) gameData.camera.target.y -= 7.f * deltaTime;
+	#pragma endregion 
+
 	#pragma region drawBlocks
 	for (int y = 0; y < gameData.gameMap.h; y++)
 		for (int x = 0; x < gameData.gameMap.w; x++)
@@ -40,7 +60,7 @@ bool updateGame()
 			auto& b = gameData.gameMap.getBlocUnsafe(x, y);
 			if (b.type != Block::air)
 			{
-				float size = 32;
+				float size = 1;
 				float posX = x * size;
 				float posY = y * size;
 				DrawTexturePro(assetManager.dirt,
@@ -53,6 +73,9 @@ bool updateGame()
 		}
 	
 	#pragma endregion
+
+	EndMode2D();
+
 	return true;
 }
 void closeGame()
