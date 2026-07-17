9428c208d49d0ad94da7760ab598dcdd6a5839cc
Adrian Barrios <adrianbarrioscortez@gmail.com>
2026-07-17 15:01:49 +0200

    Testing if button press works, as well as resizing the font size of the ImGui window. Also, allowed ImGui windows to snap with each other as well as with the main window (31-34)

--- src/main.cpp
+++ src/main.cpp
@@ -4,26 +4,51 @@
 #include <rlImGui.h>
 int main()
 {
+#pragma region initialConfig
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 	SetTargetFPS(30);
 	InitWindow(800, 450, "Oilman");
+#pragma endregion
+
+#pragma region imguiMainConfig
+	// ImGui Config
 	rlImGuiSetup(true); // Required setup context. Won't work without this.
+	ImGuiIO& io = ImGui::GetIO();
+	io.FontGlobalScale = 2;
+	io.ConfigFlags = ImGuiConfigFlags_DockingEnable; // Allows us to snap ImGui windows together
+#pragma endregion
 
-	int posX = 30;
-	int posY = 200;
-	int size = 100;
-
+#pragma region mainWhileLoop
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
 		ClearBackground(BLACK);
-
+		#pragma region imGuiWindow
 		rlImGuiBegin(); // ImGui window
+
+		// Allow for docking ImGui windows without covering with a gray screen
+		// Allow for docking ImGui windows without covering with a gray screen
+		ImGui::PushStyleColor(ImGuiCol_WindowBg, {});
+		ImGui::PushStyleColor(ImGuiCol_DockingEmptyBg, {}); 
+		ImGui::DockSpaceOverViewport(ImGui::GetMainViewport()); // Allows docking on main window.
+		ImGui::PopStyleColor(2);
 
 		ImGui::Begin("Test");
 		ImGui::Text("ImGui visible test");
-		ImGui::Button("Click me");
+		if (ImGui::Button("Button"))
+		{
+			std::cout << "Button pressed" << std::endl;
+		}
+		if (ImGui::Button("Button 2")) // Can't have 2 widgets with the same Id.
+		{
+			std::cout << "Second button pressed" << std::endl;
+		}
+		ImGui::Separator();
+		ImGui::NewLine();
+		static float a = 0;
+		ImGui::SliderFloat("slider", &a, 0, 1);
 		ImGui::End();
+		#pragma endregion imGuiWindow
 
 		Color c;
 		c.r = 255;
@@ -32,13 +57,11 @@ int main()
 		c.a = 255;
 
 		DrawText("Oilman", 350, 50, 50, RED);
-		DrawRectangle(posX, posY, size, size, BLUE);
-		posX += 1;
-
 		rlImGuiEnd();
 
 		EndDrawing();
 	}
+#pragma endregion
 
 	rlImGuiShutdown();
 	CloseWindow();
